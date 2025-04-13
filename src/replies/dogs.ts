73852f5b9ef8a166b40347470abc7a8de199277b
int <huanginch329@gmail.com>
2025-04-13 20:15:53 +0800

    @feat: add dogs commands

--- src/replies/dogs.ts
+++ src/replies/dogs.ts
@@ -2,9 +2,9 @@ import path from "path";
 import { Message } from "discord.js";
 import { sendReply } from "../utils/replyHelper";
 
-const keywords = ["錒葉", "四沁錒葉", "阿鳳"];
+const keywords = ["錒葉", "四沁錒葉", "阿鳳", "哈士堡"];
 
-const replies = ["過來"];
+const replies = ["過來", "乖狗狗"];
 
 export default {
   keywords,
