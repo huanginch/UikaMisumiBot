66e8d02c3234a0d92eecf0c665c153130dc5da91
int <huanginch329@gmail.com>
2025-04-17 00:13:46 +0800

    @feat: update command

--- src/replies/dogs.ts
+++ src/replies/dogs.ts
@@ -2,7 +2,7 @@ import path from "path";
 import { Message } from "discord.js";
 import { sendReply } from "../utils/replyHelper";
 
-const keywords = ["錒葉", "阿鳳", "哈士堡"];
+const keywords = ["錒葉", "阿鳳", "哈士堡", "方塊"];
 
 const replies = ["過來", "乖狗狗", {text: "...", timeout: true}];
 
