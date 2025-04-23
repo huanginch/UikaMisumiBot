61a85a64400980505f65146f81e10850d0c1df3b
int <huanginch329@gmail.com>
2025-04-23 20:18:30 +0800

    @feat: update 八芒星ダンス url

--- src/replies/dasozu.ts
+++ src/replies/dasozu.ts
@@ -5,7 +5,7 @@ import { sendReply } from "../utils/replyHelper";
 const keywords = ["八芒星ダンス", "八芒星"];
 
 const replies = [
-  "https://youtu.be/QtWdokSWYpo?si=BkitHw5gHVKAi4RM",
+  "https://youtu.be/tevtnb427Mw?si=9xJjtLamrdDhKG_q",
   "赤紅的象，獅子，熊，go-go insane",
 ];
 
