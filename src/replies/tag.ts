a849e314d180fcad8797a50ef6cf0144f16c8300
int <huanginch329@gmail.com>
2025-05-24 22:02:32 +0800

    @feat: Update tag command

--- src/replies/tag.ts
+++ src/replies/tag.ts
@@ -1,7 +1,7 @@
 import { Message } from "discord.js";
 import { removeRole, sendReply } from "../utils/replyHelper";
 
-const keywords = ["<@1224398179106295859>"];
+const keywords = [/\<@1224398179106295859\>/];
 
 const replies = [{ text: "真是隻沒禮貌的狗，別回來了", removeRole: "初華(初音)的狗" }];
 
