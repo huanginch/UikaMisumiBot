af0ddf1b3dacaa0a5cabd8a705db9b44c0d6d635
int <huanginch329@gmail.com>
2025-06-25 23:55:36 +0800

    @feat: add birth command

---
+++ src/replies/birth.ts
@@ -0,0 +1,13 @@
+import { Message } from "discord.js";
+import { sendReply } from "../utils/replyHelper";
+
+const keywords = [/^生日快樂+$/];
+
+const replies = [{ text: "謝謝你" }, {"text": "謝謝你，也給你的回禮", timeout: 86400_000 }];
+
+export default {
+  keywords,
+  async execute(message: Message) {
+    await sendReply(message, replies);
+  },
+};
