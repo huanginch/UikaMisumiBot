bcaaaf2f32a1d898ea35757fe7c7cd10d927cb19
int <huanginch329@gmail.com>
2025-06-26 00:06:07 +0800

    @fix: timeout in birth

--- src/replies/birth.ts
+++ src/replies/birth.ts
@@ -1,13 +1,23 @@
 import { Message } from "discord.js";
-import { sendReply } from "../utils/replyHelper";
+import { sendReply, timeoutUser } from "../utils/replyHelper";
 
 const keywords = [/^生日快樂+$/];
 
-const replies = [{ text: "謝謝你" }, { text: "謝謝你，也給你的回禮", timeout: 86400_000 }];
+const replies = [
+  { text: "謝謝你" },
+  { text: "謝謝你，也給你的回禮", timeout: 86400_000 },
+];
 
 export default {
   keywords,
   async execute(message: Message) {
-    await sendReply(message, replies);
+    const selected =await sendReply(message, replies);
+
+    if (selected.timeout) {
+      const duration =
+        typeof selected.timeout === "number" ? selected.timeout : undefined;
+
+      await timeoutUser(message, duration);
+    }
   },
 };
